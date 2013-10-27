f55622737466cb268b0772b74a185d19fc7dce26
Triệu Khang <hoangtrieukhang@gmail.com>
2013-10-27 22:41:45 +0700

    Initial Commit

---
+++ TweetCard/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  TweetCard
+//
+//  Created by Triệu Khang on 27/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetCard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TweetCard
+//
+//  Created by Triệu Khang on 27/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
